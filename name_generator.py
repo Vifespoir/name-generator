463045fe602db7141a8f363243dd12bae745d1ed
vifespoir <vifespoir@gmail.com>
2016-09-08 10:26:52 +0200

    write request info into db

--- name_generator.py
+++ name_generator.py
@@ -96,7 +96,7 @@ def show_names():
             names.append(name)
 
     try:
-        cur = db.execute('select number, ageL, ageH from info order by id desc')
+        cur = db.execute('select nb_names, ageL, ageH, caps from info order by id desc')
         infoDB = cur.fetchall()
     except sqlite3.OperationalError as e:
         errors.append(e)
